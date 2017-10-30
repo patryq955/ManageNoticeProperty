c063227278a7e335504c82e4def312c5edb908ee
Patryk Lehmann <palehmann95@gmail.com>
2017-10-30 21:50:52 +0100

    Add app

---
+++ ManageNoticeProperty/ManageNoticeProperty/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ManageNoticeProperty.Startup))]
+namespace ManageNoticeProperty
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
